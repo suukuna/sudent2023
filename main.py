97b8081572080d323693177b5a38754ffd56448f
“Jorik” <georgiys777@gmail.com>
2023-05-02 16:06:41 +0200

    add changes main.py

--- main.py
+++ main.py
@@ -20,12 +20,16 @@ import library
 #
 
 def main():
-    schedule.every().day.at("15:53", "Europe/Amsterdam").do(library.greeting)
+    schedule.every().monday.at("08:15").do(library.greeting)
+    schedule.every().tuesday.at("08:15").do(library.greeting)
+    schedule.every().wednesday.at("08:15").do(library.greeting)
+    schedule.every().thursday.at("08:15").do(library.greeting)
+    schedule.every().friday.at("08:15").do(library.greeting)
 
     schedule.every().day.at("00:30", "Europe/Amsterdam").do(library.farewell)
     while True:
         schedule.run_pending()
-        time.sleep(60)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
