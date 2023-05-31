cacf33709a55eb2369d5d007d9eecca0613b3cb6
“Jorik” <georgiys777@gmail.com>
2023-05-31 18:43:18 +0200

    updated current file

--- hw_inharitance.py
+++ hw_inharitance.py
@@ -1,31 +1,46 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class CONFIG(Enum):
+    TANK_VOLUME = 60
 
 
 class Vehicle(ABC):
 
-    def __init__(self):
+    def __init__(self, brand, fuel_remaining, speed, mileage):
+        self.brand = brand
+        self.tank_volume = CONFIG.TANK_VOLUME.value
+        self.fuel_remaining = fuel_remaining
+        self.speed = speed
+        self.mileage = mileage
+
+    def refueling(self, liters):
+        self.fuel_remaining += liters
+        if self.fuel_remaining > CONFIG.TANK_VOLUME.value:
+            self.fuel_remaining = CONFIG.TANK_VOLUME.value
+
+    @abstractmethod
+    def __str__(self):
+        return f'{self.brand}, has {self.fuel_remaining} fuel left'
+
+
+class Auto(Vehicle):
+    def __init__(self, brand, fuel_remaining, speed, mileage, passengers_number=4, airbags_presence=True):
+        super().__init__(brand, fuel_remaining, speed, mileage)
+        self.passengers_number = passengers_number
+        self.airbags_presence = True
+
+    def __str__(self):
+        return f'{self.brand}, has {self.fuel_remaining} fuel left'
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, brand, fuel_remaining, speed, mileage, tank_volume=60, cradle_presence=True):
+        super().__init__(brand, fuel_remaining, speed, mileage)
         pass
 
-
-class Auto:
-    def __init__(self, brand, tank_volume, fuel_remaining, speed, mileage):
-        self.__brand = brand
-        self.__tank_volume = tank_volume
-        self.__fuel_remaining = fuel_remaining
-        self.__speed = speed
-        self.__mileage = mileage
-        self.__passengers_number = 0
-
-
-# passengers_number, airbags_presence
-
-
-class Motorcycle:
-    def __init__(self, brand, tank_volume, fuel_remaining, speed, mileage):
-        self.__brand = brand
-        self.__tank_volume = tank_volume
-        self.__fuel_remaining = fuel_remaining
-        self.__speed = speed
-        self.__mileage = mileage
-
-# cradle_presence
+car = Auto(brand='Lamborghini', fuel_remaining=20, speed=260, mileage=10000)
+print(car)
+car.refueling(500)
+print(car)
