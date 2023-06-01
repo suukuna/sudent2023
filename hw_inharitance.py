ce059f69a235540528b2f89bf637ad3b32022fee
“Jorik” <georgiys777@gmail.com>
2023-06-02 00:23:06 +0200

    updated current file

--- hw_inharitance.py
+++ hw_inharitance.py
@@ -31,7 +31,19 @@ class Auto(Vehicle):
         self.passengers_number = passengers_number
         self.airbags_presence = True
 
+    def transfusion(self, liters, other: Vehicle):
+        if self.fuel_remaining > 0:
+            self.fuel_remaining -= liters
+            other.fuel_remaining += liters
+            if self.fuel_remaining >= CONFIG.TANK_VOLUME.value:
+                print('Can not continue, tank is full')
+            else:
+                print('Still filling the tank')  #?
+        else:
+            print('Not enough fuel:(')
+
     def __str__(self):
+        # if vizov funkcii refueling otvecaht' eto, else vernut' drugoy zagotovleniy text?
         return f'{self.brand}, has {self.fuel_remaining} fuel left'
 
 
@@ -40,7 +52,28 @@ class Motorcycle(Vehicle):
         super().__init__(brand, fuel_remaining, speed, mileage)
         pass
 
+    def transfusion(self, liters, other: Vehicle):
+        if self.fuel_remaining > 0:
+            self.fuel_remaining -= liters
+            other.fuel_remaining += liters
+            if self.fuel_remaining >= CONFIG.TANK_VOLUME.value:
+                print('Can not continue, tank is full')
+            else:
+                print('Still filling the tank')  #?
+        else:
+            print('Not enough fuel:(')
+
+    def __str__(self):
+        return f'{self.brand}, has {self.fuel_remaining} fuel left'
+
+
 car = Auto(brand='Lamborghini', fuel_remaining=20, speed=260, mileage=10000)
-print(car)
-car.refueling(500)
-print(car)
+motorbike = Motorcycle(brand='Lamborghini', fuel_remaining=15, speed=240, mileage=1000)
+# # print(car)
+# car.refueling(500)
+# print(car)
+# print(motorbike)
+# motorbike.refueling(500)
+# print(motorbike)
+car.transfusion(motorbike)
+
