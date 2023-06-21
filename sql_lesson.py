4ff06f806f6af500626e6d4774cb126e1ad42f62
“Jorik” <georgiys777@gmail.com>
2023-06-21 23:26:52 +0200

    hw start

--- sql_lesson.py
+++ sql_lesson.py
@@ -1,24 +1,24 @@
 import sqlite3
 
-with sqlite3.connect('test.db') as connection:
+with sqlite3.connect('products.db') as connection:
     cursor = connection.cursor()
-    cursor.execute()
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS country(
+        CREATE TABLE IF NOT EXISTS product(
             id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
             name TEXT NOT NULL,
-            population INTEGER 
+            price INTEGER NOT NULL,
+            description STRING 
         );
     """)
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS customer(
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            name TEXT NOT NULL,
-            address TEXT,
-            email TEXT,
-            country_id INTEGER NOT NULL DEFAULT 0,
-            balance REAL NOT NULL DEFAULT 0,
-            FOREIGN KEY (country_id) REFERENCES country(id)
-        );
-    """)
+    # cursor.execute("""
+    #     CREATE TABLE IF NOT EXISTS customer(
+    #         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    #         name TEXT NOT NULL,
+    #         address TEXT,
+    #         email TEXT,
+    #         country_id INTEGER NOT NULL DEFAULT 0,
+    #         balance REAL NOT NULL DEFAULT 0,
+    #         FOREIGN KEY (country_id) REFERENCES country(id)
+    #     );
+    # """)
