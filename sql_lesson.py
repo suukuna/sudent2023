26ef858fe70228a43f5b89304075b4ad3b6bb577
“Jorik” <georgiys777@gmail.com>
2023-06-22 13:16:11 +0200

    finished

--- sql_lesson.py
+++ sql_lesson.py
@@ -11,14 +11,49 @@ with sqlite3.connect('products.db') as connection:
             description STRING 
         );
     """)
-    # cursor.execute("""
-    #     CREATE TABLE IF NOT EXISTS customer(
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    #         name TEXT NOT NULL,
-    #         address TEXT,
-    #         email TEXT,
-    #         country_id INTEGER NOT NULL DEFAULT 0,
-    #         balance REAL NOT NULL DEFAULT 0,
-    #         FOREIGN KEY (country_id) REFERENCES country(id)
-    #     );
+
+    # values = [
+    #     ('Tea', 12, 'hot drink'),
+    #     ('Milk', 12, 'BEST DRINK IN THE ENTIRE WORLD'),
+    #     ('Cola Soda', 28, 'Cheap Cola alternative'),
+    #     ('Orange Fresh', 150, 'Homemade orange juice'),
+    #     ('Dry Wine', 123, 'Alcohol sugar less drink'),
+    #     ('VODKA', 134, 'Hard alcohol drink'),
+    # ]
+
+    # cursor.executemany(
+    #     """
+    #     INSERT INTO product (name, price, description)
+    #     VALUES (?, ?, ?)
+    #     """,
+    #     values,
+    #  )
+
+    # data = cursor.execute("""
+    #     SELECT *
+    #     FROM product
     # """)
+    #
+    # print(data.fetchmany(5))
+
+    # data = cursor.execute("""
+    #     SELECT *
+    #     FROM product
+    #     WHERE
+    #         price > 100 AND description like '%sugar%'
+    #     ;
+    # """)
+    # print(data.fetchall())
+
+    # cursor.execute("""
+    #     UPDATE product
+    #     SET
+    #         price = price + 12
+    #     WHERE
+    #         price < 10;
+    # """)
+
+    cursor.execute("""
+        DELETE FROM product
+        WHERE name LIKE '%Cola%';
+     """)
